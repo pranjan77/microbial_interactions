0821ad048ee87b47e3923a3f087b49a4275a1777
Priya Ranjan <pranjan77@gmail.com>
2023-07-13 22:06:37 -0400

    updated member_models instances to take care of failing tests

--- test/microbial_interactions_server_test.py
+++ test/microbial_interactions_server_test.py
@@ -65,7 +65,7 @@ class microbial_interactionsTest(unittest.TestCase):
         # self.assertEqual(ret[...], ...) or other unittest methods
         params = {
             'workspace_name':'pranjan77:narrative_1650064809467',
-            "input_models":[ "137367/1887/1", "137367/2417/1", "137367/1925/1" ],
+            "member_models":[ "137367/1887/1", "137367/2417/1", "137367/1925/1" ],
             "media": ["137367/1656/1"]
         }
         ret = self.serviceImpl.run_microbial_interactions(self.ctx, params)
